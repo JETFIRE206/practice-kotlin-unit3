9706c8ede0db2eab5e10fe282831865e7c77c385
Jason Lischke <jasonlischke@comcast.net>
2022-11-09 12:08:40 -0800

    Added Navigation Drawer to app

--- app/src/main/java/com/example/unit3/MainActivity.kt
+++ app/src/main/java/com/example/unit3/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.unit3
 
 import android.os.Bundle
+import android.text.TextUtils.replace
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.example.unit3.databinding.ActivityMainBinding
 import com.example.unit3.view.PracticeHighOrderFunc
 import com.example.unit3.view.PracticeLambdasFragment
+import com.google.android.material.navigation.NavigationView
 
 // Generally, your fragment must be embedded within an AndroidX "FragmentActivity" to contribute a
 // portion of UI to that activity's layout. "FragmentActivity" is the base class for
@@ -15,6 +20,7 @@ import com.example.unit3.view.PracticeLambdasFragment
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,11 +34,43 @@ class MainActivity : AppCompatActivity() {
         binding.buttonHigherOrderFunc.setOnClickListener {
             replaceFragment(PracticeHighOrderFunc())
         }
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+
+            it.isChecked = true
+
+            when (it.itemId) {
+                R.id.nav_lambda -> replaceFragment(PracticeLambdasFragment())
+                R.id.nav_high_order_func -> replaceFragment(PracticeHighOrderFunc())
+                else -> replaceFragment(PracticeLambdasFragment())
+            }
+
+            drawerLayout.closeDrawers()
+
+            true
+        }
     }
 
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.commit {
             replace(binding.fragmentContainerView.id, fragment)
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
