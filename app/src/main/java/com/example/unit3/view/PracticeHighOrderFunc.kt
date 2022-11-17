f6c1b6f47cdcc367189028083175c88e852a756e
Jason Lischke <jasonlischke@comcast.net>
2022-11-17 12:09:15 -0800

    Added more practice examples

--- app/src/main/java/com/example/unit3/view/PracticeHighOrderFunc.kt
+++ app/src/main/java/com/example/unit3/view/PracticeHighOrderFunc.kt
@@ -23,18 +23,30 @@ class PracticeHighOrderFunc : Fragment(R.layout.fragment_practice_high_order_fun
         //return super.onCreateView(inflater, container, savedInstanceState)
         _binding = FragmentPracticeHighOrderFuncBinding.inflate(inflater, container, false)
 
-        val people = listOf("Fred", "Ann", "Barbara", "Joe")
+        val people = listOf("Fred", "Ann", "Barbara", "Joe", "Bo", "Stephanie")
         appendText("Original List:")
         //people.forEach { appendText(it) }
         appendText(people.toString())
+
+        // Print people who's name contains the letter 'e'.
+        appendText("\nFilter: (names containing letter 'e')")
+        appendText(people.filter {it.contains("e")}
+            .toString())
+
+        // Print people who's names are longer than 4 characters.
+        appendText("\nFilter: (Names longer than 4 characters)")
+        appendText(people.filter { it.length > 4}
+            .toString())
 
         // Note: To compare two objects for sorting, the convention is to return a value less than 0
         // if the first object is less than the second, 0 if they are equal, and a value greater than
         // 0 if the first object is greater than the second.
         appendText("\nSorted by Length:")
-        //people.sortedWith { str1: String, str2: String -> str1.length - str2.length}
-        //    .forEach { appendText(it) }
         appendText(people.sortedWith { str1: String, str2: String -> str1.length - str2.length }
+            .toString())
+
+        appendText("\nSorted by Length: (longest to shortest)")
+        appendText(people.sortedWith { str1: String, str2: String -> str2.length - str1.length }
             .toString())
 
         return binding.root
--- app/src/main/java/com/example/unit3/view/PracticeLambdasFragment.kt
+++ app/src/main/java/com/example/unit3/view/PracticeLambdasFragment.kt
@@ -27,6 +27,7 @@ class PracticeLambdasFragment : Fragment(R.layout.fragment_practice_lambdas) {
         val number: Int = 5
         //val triple: (Int) -> Int = { a: Int -> a * 3}
         val triple: (Int) -> Int = { it * 3 }
+        appendText("Lambda: ('Int = { it * 3 }')")
         appendText("Triple $number = ${triple(number)}")
 
         // Lambda Functions
@@ -37,29 +38,42 @@ class PracticeLambdasFragment : Fragment(R.layout.fragment_practice_lambdas) {
         // A lambda in all its glory, with explicit types everywhere. The lambda is the part in
         // curly braces, which is assigned to a variable of type (String) -> String (a function type).
         val upperCase1: (String) -> String = { str: String -> str.uppercase() }
+        appendText("\nLambda in all its glory:")
+        appendText("('val upperCase1: (String) -> String = { str: String -> str.uppercase() }'))")
         appendText("UpperCase1: ${upperCase1("hello")}")
 
         // Type inference inside lambda: the type of the lambda parameter is inferred from the type
         // of the variable it's assigned to.
         val upperCase2: (String) -> String = { str -> str.uppercase() }
+        appendText("\nType inference inside lambda:")
+        appendText("('val upperCase2: (String) -> String = { str -> str.uppercase() }')")
         appendText("UpperCase2: ${upperCase2("hello")}")
 
         // Type inference outside lambda: the type of the variable is inferred from the type of the
         // lambda parameter and return value.
         val upperCase3 = { str: String -> str.uppercase() }
+        appendText("\nType inference outside lambda:")
+        appendText("('val upperCase3 = { str: String -> str.uppercase() }')")
         appendText("UpperCase3: ${upperCase3("hello")}")
 
         // You cannot do both together, the compiler has no chance to infer the type that way.
         //val uppercase4 = { str -> str.uppercase() }
+        appendText("\nType inference inside and outside lambda together IS NOT POSSIBLE.")
+        appendText("'val uppercase4 = { str -> str.uppercase() }'")
+        appendText("Cannot infer a type for this parameter. Please specify it explicitly")
 
         // For lambdas with a single parameter, you don't have to explicitly name it. Instead, you
         // can use the implicit it variable. This is especially useful when the type of it can be
         // inferred (which is often the case).
         val upperCase5: (String) -> String = { it.uppercase() }
+        appendText("\nSingle Parameter Lambda:")
+        appendText("('val upperCase5: (String) -> String = { it.uppercase() }')")
         appendText("UpperCase5: ${upperCase5("hello")}")
 
         // If your lambda consists of a single function call, you may use function pointers (::).
         val upperCase6: (String) -> String = String::uppercase
+        appendText("\nFunction pointers:")
+        appendText("('val upperCase6: (String) -> String = String::uppercase')")
         appendText("UpperCase6: ${upperCase6("hello")}")
 
         return binding.root
